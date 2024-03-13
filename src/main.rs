0212df2de1ce7d8ea3db0d0a5f32d9b1dc2360be
sparqxi <146112627+sparqxi@users.noreply.github.com>
2024-03-13 09:02:02 -0700

    code refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use graphics::{clear, math::{add, mul_scalar, Vec2d}, rectangle};
+use graphics::{clear, rectangle};
 use piston_window::{PistonWindow, WindowSettings};
-use rand::prelude::*;
+use world::World;
 use std::{
     alloc::{GlobalAlloc, System},
     time::Instant,
 };
+mod world;
+mod particle;
 
 #[global_allocator]
 static ALLOCATOR: ReportingAllocator = ReportingAllocator;
@@ -23,103 +25,6 @@ unsafe impl GlobalAlloc for ReportingAllocator {
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: std::alloc::Layout) {
         System.dealloc(ptr, layout);
-    }
-}
-
-struct World {
-    current_turn: u64,
-    particles: Vec<Box<Particle>>,
-    height: f64,
-    width: f64,
-    rng: ThreadRng,
-}
-
-impl World {
-    fn new(width: f64, height: f64) -> World {
-        World {
-            current_turn: 0,
-            particles: Vec::<Box<Particle>>::new(),
-            height,
-            width,
-            rng: thread_rng(),
-        }
-    }
-    fn add_shapes(&mut self, n: i32) {
-        for _ in 0..n.abs() {
-            let particle = Particle::new(&self);
-            let boxed_particles = Box::new(particle);
-            self.particles.push(boxed_particles);
-        }
-    }
-
-    fn remove_shapes(&mut self, n: i32) {
-        for _ in 0..n.abs() {
-            let mut to_delete = None;
-            let particle_iter = self.particles.iter().enumerate();
-            for (i, particle) in particle_iter {
-                if particle.color[3] < 0.02 {
-                    to_delete = Some(i);
-                }
-                break;
-            }
-            if let Some(i) = to_delete {
-                self.particles.remove(i);
-            } else {
-                self.particles.remove(0);
-            }
-        }
-    }
-
-    fn update(&mut self) {
-        let n = self.rng.gen_range(-3..=3);
-        if n > 0 {
-            self.add_shapes(n);
-        } else {
-            self.remove_shapes(n)
-        }
-
-        self.particles.shrink_to_fit();
-        for shape in &mut self.particles {
-            shape.update()
-        }
-        self.current_turn += 1;
-    }
-}
-
-struct Particle {
-    height: f64,
-    width: f64,
-    position: Vec2d<f64>,
-    velocity: Vec2d<f64>,
-    acceleration: Vec2d<f64>,
-    color: [f32; 4],
-}
-
-impl Particle {
-    fn new(world: &World) -> Particle {
-        let mut rng = thread_rng();
-        let x = rng.gen_range(0.0..=world.width);
-        let y = world.height;
-        let x_velocity = 0.0;
-        let y_velocity = rng.gen_range(-2.0..0.0);
-        let x_acceleration = 0.0;
-        let y_acceleration = rng.gen_range(0.0..0.15);
-
-        Particle {
-            height: 4.0,
-            width: 4.0,
-            position: [x, y].into(),
-            velocity: [x_velocity, y_velocity].into(),
-            acceleration: [x_acceleration, y_acceleration].into(),
-            color: [1.0, 1.0, 1.0, 0.99],
-        }
-    }
-
-    fn update(&mut self) {
-        self.velocity = add(self.velocity, self.acceleration);
-        self.position = add(self.position, self.velocity);
-        self.acceleration = mul_scalar(self.acceleration, 0.7);
-        self.color[3] *= 0.995;
     }
 }
 
